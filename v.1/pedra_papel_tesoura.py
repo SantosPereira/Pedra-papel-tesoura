22a84b63d1e8c57ef2bbfd019e73e69112ff7dc0
SantosPereira <pedrohenriquelemam@gmail.com>
2021-03-20 16:53:08 -0300

    Update pedra_papel_tesoura.py

--- v.1/pedra_papel_tesoura.py
+++ v.1/pedra_papel_tesoura.py
@@ -1,12 +1,8 @@
 """
 TO DO: Emitir sons quando houver vitória ou derrota no jogo
 """
-
-
 from random import randint as random
 from time import sleep
-#from pisca import pisca
-
 
 sleep(1)
 print('''\n\n\n
@@ -25,19 +21,20 @@ sleep(1.5)
 def play():
     print('Escolha:\n[1] Para Pedra\n[2] Para Papel\n[3] Para Tesoura\n\n\n\n')
     sleep(1)
-    
-########         fica rodando infinitamente      ----     Provisório
-#   while True:   
-#       print('\033[1;32m>>\033[0;0m', end='', flush=True)
-#       sleep(0.5)
-#       print('\x1b[2K\r', end='', flush=True)
-#       sleep(0.5)
-#######################################################################################
+    '''
+    # --> fica rodando infinitamente
+    while True:
+        print('\x1b[2K\r', end='', flush=True)
+        print('\033[1;32m>>\033[0;0m', end='', flush=True)
+        sleep(0.5)
+        print('\x1b[2K\r', end='  ', flush=True)
+        sleep(0.5)'''
 
-#### Minha intenção no trecho de código acima era fazer '>>' ficar piscando na tela e quando fosse digitado algo no input ele ler a entrada, porém o input para o programa
+    ''' Minha intenção no trecho de código acima era fazer '>>' ficar piscando na tela e
+    quando fosse digitado algo no input ele ler a entrada, porém o input para o programa'''
 
     player1 = input('\033[1;32m>>\033[0;0m')
-    player2 = random(1,3) ### [1] é pedra, [2] é papel, [3] é tesoura
+    player2 = random(1,3) # [1] é pedra, [2] é papel, [3] é tesoura
    
 
     if player1 == '1':
